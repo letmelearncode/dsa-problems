bb598e1a8d5d89bb9e272498a75621cff562f55c
letmelearncode <iamabhi3913@gmail.com>
2021-09-22 11:36:24 +0530

    iterative in-order traversal implemented

--- src/tree/binarytree/BinaryTreeImpl.java
+++ src/tree/binarytree/BinaryTreeImpl.java
@@ -1,189 +1,259 @@
 package tree.binarytree;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class BinaryTreeImpl {
 
-  private Node root;
-  private int size;
+    private Node root;
+    private int size;
 
-  private class Node {
+    private class Node {
 
-    int data;
-    Node left;
-    Node right;
+        int data;
+        Node left;
+        Node right;
 
-    public Node(int data, Node left, Node right) {
-      this.data = data;
-      this.left = left;
-      this.right = right;
-    }
-  }
-
-  public BinaryTreeImpl() {
-    Scanner sc = new Scanner(System.in);
-    this.root = takeInput(sc, null, false);
-  }
-
-  private Node takeInput(Scanner sc, Node parent, boolean isLeftOrRight) {
-    if (parent == null) {
-      System.out.println("Enter the data of the root node");
-    } else {
-      if (isLeftOrRight) {
-        System.out.println("Enter the data for left Node with parent node as " + parent.data);
-      } else {
-        System.out.println("Enter the data for right node with parent node as " + parent.data);
-      }
+        public Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
     }
 
-    Node node = new Node(sc.nextInt(), null, null);
-    this.size++;
-    boolean choice = false;
-    System.out.println("Enter true if the node " + node.data + " has left node");
-    choice = sc.nextBoolean();
-    if (choice) {
-      node.left = takeInput(sc, node, true);
+    public BinaryTreeImpl() {
+        Scanner sc = new Scanner(System.in);
+        this.root = takeInput(sc, null, false);
     }
 
-    choice = false;
-    System.out.println("Enter true if the node " + node.data + " has right node");
-    choice = sc.nextBoolean();
-    if (choice) {
-      node.right = takeInput(sc, node, false);
+    private Node takeInput(Scanner sc, Node parent, boolean isLeftOrRight) {
+        if (parent == null) {
+            System.out.println("Enter the data of the root node");
+        } else {
+            if (isLeftOrRight) {
+                System.out.println("Enter the data for left Node with parent node as " + parent.data);
+            } else {
+                System.out.println("Enter the data for right node with parent node as " + parent.data);
+            }
+        }
+
+        Node node = new Node(sc.nextInt(), null, null);
+        this.size++;
+        boolean choice = false;
+        System.out.println("Enter true if the node " + node.data + " has left node");
+        choice = sc.nextBoolean();
+        if (choice) {
+            node.left = takeInput(sc, node, true);
+        }
+
+        choice = false;
+        System.out.println("Enter true if the node " + node.data + " has right node");
+        choice = sc.nextBoolean();
+        if (choice) {
+            node.right = takeInput(sc, node, false);
+        }
+
+        return node;
     }
 
-    return node;
-  }
-
-  public int size() {
-    return this.size;
-  }
-
-  public void display() {
-    this.display(this.root);
-
-  }
-
-  public int height() {
-    return this.height(this.root);
-  }
-
-  public void preOrderTraversal() {
-    this.preOrderTraversal(this.root);
-  }
-
-  public void inOrderTraversal() {
-    this.inOrderTraversal(this.root);
-  }
-
-  public void postOrderTraversal() {
-    this.postOrderTraversal(this.root);
-  }
-
-  public void levelOrderTraversal() {
-    this.levelOrderTraversal(this.root);
-  }
-
-  public boolean find(int data) {
-    return this.find(this.root, data);
-  }
-
-  private boolean find(Node node, int data) {
-
-    if (node == null) {
-      return false;
-    }
-    System.out.println(node.data);
-    if (node.data == data) {
-      return true;
+    public int size() {
+        return this.size;
     }
 
-    return (find(node.left, data) || find(node.right, data));
+    public void display() {
+        this.display(this.root);
 
-  }
-
-
-  private void levelOrderTraversal(Node node) {
-   LinkedList<Node> nodes = new LinkedList<>();
-   nodes.add(node);
-
-   while(!nodes.isEmpty()){
-     Node removedNode = nodes.removeFirst();
-     System.out.print(removedNode.data+" ");
-     if(removedNode.left!=null){
-       nodes.addLast(removedNode.left);
-     }
-     if(removedNode.right!=null){
-       nodes.addLast(removedNode.right);
-     }
-   }
-    System.out.println("");
-  }
-
-  private void postOrderTraversal(Node node) {
-    if (node == null) {
-      return;
     }
-    this.postOrderTraversal(node.left);
-    this.postOrderTraversal(node.right);
-    System.out.print(node.data + " ");
 
-  }
-
-  private void inOrderTraversal(Node node) {
-    if (node == null) {
-      return;
+    public int height() {
+        return this.height(this.root);
     }
-    this.inOrderTraversal(node.left);
-    System.out.print(node.data + " ");
-    this.inOrderTraversal(node.right);
-  }
 
-  private void preOrderTraversal(Node node) {
-    if (node == null) {
-      return;
+    public void preOrderTraversal() {
+        this.preOrderTraversal(this.root);
     }
-    System.out.print(node.data + " ");
 
-    preOrderTraversal(node.left);
-    preOrderTraversal(node.right);
-  }
-
-  private int height(Node node) {
-    if (node == null) {
-      return -1;
+    public void inOrderTraversal() {
+        this.inOrderTraversal(this.root);
     }
-    int leftTreeHeight = height(node.left);
-    int rightTreeHeight = height(node.right);
-    if (leftTreeHeight >= rightTreeHeight) {
-      return leftTreeHeight + 1;
-    } else {
-      return rightTreeHeight + 1;
+
+    public void postOrderTraversal() {
+        this.postOrderTraversal(this.root);
     }
-  }
 
-  private void display(Node root) {
-    String str = "";
-
-    if (root != null) {
-      str += root.data;
-
-      if (root.left != null) {
-        str = root.left.data + "=>" + str;
-      } else {
-        str = "END" + "=>" + str;
-      }
-      if (root.right != null) {
-        str = str + "<=" + root.right.data;
-      } else {
-        str = str + "<=" + "END";
-      }
-      System.out.println(str);
-      this.display(root.left);
-      this.display(root.right);
+    public void levelOrderTraversal() {
+        this.levelOrderTraversal(this.root);
     }
-  }
+
+    public void iterativeInOrderTraversal() {
+        this.iterativeInOrderTraversal(this.root);
+    }
+
+    public int sumOfLeafNodes() {
+        return this.sumOfLeafNodes(this.root);
+    }
+
+    private int sumOfLeafNodes(Node node){
+
+        if(node == null){
+            return 0;
+        }
+        if(node.left==null && node.right==null)
+            return node.data;
+
+        return sumOfLeafNodes(node.left) + sumOfLeafNodes(node.right);
+    }
+/*
+    private int sumOfLeafNodes(Node node) {
+
+
+        Stack<Node> nodeStack = new Stack<>();
+        int sum = 0;
+
+        while (true) {
+            if (node != null) {
+                nodeStack.push(node);
+                node = node.left;
+            } else {
+                if (nodeStack.isEmpty())
+                    break;
+                node = nodeStack.pop();
+                if(node.right==null && node.left==null){
+                    sum += node.data;
+                }
+
+                node = node.right;
+            }
+        }
+        return sum;
+    }
+*/
+
+
+    private void iterativeInOrderTraversal(Node node) {
+
+        Stack<Node> nodeStack = new Stack<>();
+        String output = "";
+
+        while (true) {
+            if (node != null) {
+                nodeStack.push(node);
+                node = node.left;
+            } else {
+                if (!nodeStack.isEmpty()) {
+                    node = nodeStack.pop();
+                    output = output + " " + node.data;
+                    node = node.right;
+                } else
+                    break;
+            }
+
+        }
+        System.out.println(output);
+    }
+
+    public boolean find(int data) {
+        return this.find(this.root, data);
+    }
+
+    private boolean find(Node node, int data) {
+
+        if (node == null) {
+            return false;
+        }
+        System.out.println(node.data);
+        if (node.data == data) {
+            return true;
+        }
+
+        return (find(node.left, data) || find(node.right, data));
+
+    }
+
+
+    private void levelOrderTraversal(Node node) {
+        LinkedList<Node> nodes = new LinkedList<>();
+        nodes.add(node);
+
+        while (!nodes.isEmpty()) {
+            Node removedNode = nodes.removeFirst();
+            System.out.print(removedNode.data + " ");
+            if (removedNode.left != null) {
+                nodes.addLast(removedNode.left);
+            }
+            if (removedNode.right != null) {
+                nodes.addLast(removedNode.right);
+            }
+        }
+        System.out.println("");
+    }
+
+    private void postOrderTraversal(Node node) {
+        if (node == null) {
+            return;
+        }
+        this.postOrderTraversal(node.left);
+        this.postOrderTraversal(node.right);
+        System.out.print(node.data + " ");
+
+    }
+
+    private void inOrderTraversal(Node node) {
+        if (node == null) {
+            return;
+        }
+        this.inOrderTraversal(node.left);
+        System.out.print(node.data + " ");
+        this.inOrderTraversal(node.right);
+    }
+
+    private void preOrderTraversal(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.data + " ");
+
+        preOrderTraversal(node.left);
+        preOrderTraversal(node.right);
+    }
+
+    private int height(Node node) {
+        if (node == null) {
+            return -1;
+        }
+        int leftTreeHeight = height(node.left);
+        int rightTreeHeight = height(node.right);
+        if (leftTreeHeight >= rightTreeHeight) {
+            return leftTreeHeight + 1;
+        } else {
+            return rightTreeHeight + 1;
+        }
+    }
+
+    private void display(Node root) {
+        String str = "";
+
+        if (root != null) {
+            str += root.data;
+
+            if (root.left != null) {
+                str = root.left.data + "=>" + str;
+            } else {
+                str = "END" + "=>" + str;
+            }
+            if (root.right != null) {
+                str = str + "<=" + root.right.data;
+            } else {
+                str = str + "<=" + "END";
+            }
+            System.out.println(str);
+            this.display(root.left);
+            this.display(root.right);
+        }
+    }
+
 
 }
 
@@ -195,19 +265,22 @@ public class BinaryTreeImpl {
 //level order = 50 25 45 38 48 85 60 18
 class Driver {
 
-  public static void main(String[] args) {
-    BinaryTreeImpl tree = new BinaryTreeImpl();
-    System.out.println(tree.size());
-    tree.display();
-    System.out.println(tree.height());
-    tree.preOrderTraversal();
-    System.out.println("");
-    System.out.println(tree.find(70));
-    tree.inOrderTraversal();
-    System.out.println("");
-    tree.postOrderTraversal();
-    System.out.println("");
-    tree.levelOrderTraversal();
+    public static void main(String[] args) {
+        BinaryTreeImpl tree = new BinaryTreeImpl();
+        /*System.out.println(tree.size());
+        tree.display();
+        System.out.println(tree.height());
+        tree.preOrderTraversal();
+        System.out.println("");
+        System.out.println(tree.find(70));*/
+        tree.inOrderTraversal();
+         /* System.out.println("");
+        tree.postOrderTraversal();
+        System.out.println("");
+        tree.levelOrderTraversal();*/
+        System.out.println("");
+        tree.iterativeInOrderTraversal();
+        System.out.println(tree.sumOfLeafNodes());
 
-  }
+    }
 }
