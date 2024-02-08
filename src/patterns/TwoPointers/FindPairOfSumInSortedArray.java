c722fca8f5fc311c09e19a7d3492059d89cbc937
letmelearncode <iamabhi3913@gmail.com>
2024-02-08 13:31:27 +0530

    added sliding window pattern problems

---
+++ src/patterns/TwoPointers/FindPairOfSumInSortedArray.java
@@ -0,0 +1,34 @@
+package patterns.TwoPointers;
+
+
+/**
+ * Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to
+ * the given target.
+ */
+public class FindPairOfSumInSortedArray {
+
+  public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4, 5, 6};
+    int sum = 10;
+    int[] result = findPairOfSum(arr, sum);
+    System.out.println("Answer : "+result[0]+" "+result[1]);
+  }
+
+  private static int[] findPairOfSum(int[] arr, int sum) {
+    int[] solArr = new int[2];
+    int left = 0;
+    int right = arr.length - 1;
+    while (left <= right) {
+      if (arr[left] + arr[right] < sum) {
+        left++;
+      } else if (arr[left] + arr[right] > sum) {
+        right--;
+      } else {
+        solArr[0] = arr[left];
+        solArr[1] = arr[right];
+        break;
+      }
+    }
+    return solArr;
+  }
+}
