3df8a4ec69ff2dcbffa32da01ea46c0dc4361f49
letmelearncode <iamabhi3913@gmail.com>
2024-02-13 23:12:00 +0530

    added in place duplicate removal code

--- src/patterns/TwoPointers/FindPairOfSumInSortedArray.java
+++ src/patterns/TwoPointers/FindPairOfSumInSortedArray.java
@@ -9,26 +9,29 @@ public class FindPairOfSumInSortedArray {
 
   public static void main(String[] args) {
     int[] arr = {1, 2, 3, 4, 5, 6};
-    int sum = 10;
+    int sum = 5;
     int[] result = findPairOfSum(arr, sum);
     System.out.println("Answer : "+result[0]+" "+result[1]);
   }
 
   private static int[] findPairOfSum(int[] arr, int sum) {
-    int[] solArr = new int[2];
+    int[] solArr = {-1,-1};
     int left = 0;
     int right = arr.length - 1;
-    while (left <= right) {
+    while (left < right) {
       if (arr[left] + arr[right] < sum) {
         left++;
       } else if (arr[left] + arr[right] > sum) {
         right--;
-      } else {
+      }
+      else {
         solArr[0] = arr[left];
         solArr[1] = arr[right];
         break;
       }
+
     }
+
     return solArr;
   }
 }
